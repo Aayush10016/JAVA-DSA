2cabfa463c89b89ef7e11fb224e294e75de4807d
Aayush Tiwari <aayushtiwari1001@gmail.com>
2025-12-19 16:44:34 +0530

    Create code.java

---
+++ Sorting/Question/1502. Can Make Arithmetic Progression From Sequence/code.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        boolean swapped = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped){
+                break;
+            } 
+        }
+        int diff = arr[1] - arr[0];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
