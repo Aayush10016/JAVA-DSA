4b346e468bc5e52316ccd6403c75c5b332fa0ca8
Aayush Tiwari <aayushtiwari1001@gmail.com>
2025-12-20 16:36:46 +0530

    Create code.java

---
+++ Sorting/Question/628(leetcode). Maximum Product of Three Numbers/code.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maximumProduct(int[] arr) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int prod1 = arr[n - 1] * arr[n - 2] * arr[n - 3];
+        int prod2 = arr[0] * arr[1] * arr[n - 1];
+        return Math.max(prod1, prod2);
+    }
+}
