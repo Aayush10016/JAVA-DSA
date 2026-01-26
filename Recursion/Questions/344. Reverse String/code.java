b7619107adee2230fb52ec2c4ca2ecf74cbec833
Aayush Tiwari <aayushtiwari1001@gmail.com>
2026-01-26 21:43:31 +0530

    Create code.java

---
+++ Recursion/Questions/344. Reverse String/code.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+    }
+    static void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
